c336800f7e4c4f3fa1ece905243e93e20dd1838c
rockon999 <rockon999@users.noreply.github.com>
2018-07-02 11:33:42 -0400

    Add key path searching.

--- jsonStringsBackend.js
+++ jsonStringsBackend.js
@@ -1,8 +1,36 @@
 import StringsBackend from './stringsBackend';
 
 import HomeJSON from './home.json';
+import AssetsJSON from './assets.json';
 
 const DEFAULT_CONTEXT = 'default';
+
+const searchKey = function(key, json) {
+  let val = json[key];
+
+  if (typeof val === 'undefined' || val === null) {
+    let keys = key.split('.');
+
+    let currentChild = json;
+
+    for (let childKey of keys) {
+      if (
+        typeof currentChild[childKey] === 'undefined' ||
+        currentChild[childKey] === null
+      ) {
+        currentChild = null;
+        break;
+      }
+      currentChild = currentChild[childKey];
+    }
+
+    if (typeof currentChild !== 'undefined' && currentChild !== null) {
+      val = currentChild;
+    }
+  }
+
+  return val;
+};
 
 export default class JSONStringsBackend extends StringsBackend {
   getDefaultContext() {
@@ -13,7 +41,10 @@ export default class JSONStringsBackend extends StringsBackend {
     switch (context) {
       case DEFAULT_CONTEXT:
       case 'home':
-        return HomeJSON[key];
+        return searchKey(key, HomeJSON);
+      case 'assets':
+        // TODO should we error out if null?
+        return `/static/${searchKey(key, AssetsJSON)}`;
       default:
         return null;
     }
